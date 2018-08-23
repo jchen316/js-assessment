9d3da5b48cf6a7ea02a712853302432dadcbb2c6
Jchen <jcdigital316@gmail.com>
2018-08-22 23:35:32 -0700

    finished count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    let delay;
+    const count = ()=> {
+      console.log(start);
+      start++
+      if(start <= end){
+        delay = setTimeout( count , 100)
+      }
+    }
+    count();
 
+    return {
+			cancel: function() {
+				clearTimeout(delay);
+			}
+		};
+    
   }
 };
