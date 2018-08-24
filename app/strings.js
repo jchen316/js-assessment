ab2189e555779e3a56969a5bf240a42f162e7dac
Jchen <jcdigital316@gmail.com>
2018-08-23 18:16:02 -0700

    attempted strings

--- app/strings.js
+++ app/strings.js
@@ -2,7 +2,32 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
+    let output = [];
+    let temp = str.split("")
+    let counter = 0;
 
+    for(let i = 0; i<temp.length; i++){
+      if(temp[i] === temp[i-1]){
+        if(counter < amount){
+          output.push(temp[i])
+          counter++
+          continue;
+        } else {
+          counter++
+          continue;
+        }
+      } 
+      
+      if(i !== 0 && temp[i] !== temp[i-1]){
+        counter = 0;
+      }
+      else {
+  
+        output.push(temp[i]);
+        counter++
+      }
+    }
+    return output.join("")
   },
 
   wordWrap: function(str, cols) {
